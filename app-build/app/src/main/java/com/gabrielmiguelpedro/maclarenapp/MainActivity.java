8f77921bc34f62a5efe6b5b3fcf7320b831cb71b
PedroGrilo <xpereiragrilo@gmail.com>
2019-10-21 09:09:32 +0100

    updates

---
+++ app-build/app/src/main/java/com/gabrielmiguelpedro/maclarenapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.gabrielmiguelpedro.maclarenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
