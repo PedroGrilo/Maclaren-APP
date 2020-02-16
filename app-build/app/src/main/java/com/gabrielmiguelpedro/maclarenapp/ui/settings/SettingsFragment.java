ce2cb3d746abd28f71f898f09af60723b059c561
nhenheca <33757448+nhenheca@users.noreply.github.com>
2020-02-16 17:16:09 +0000

    settings

--- app-build/app/src/main/java/com/gabrielmiguelpedro/maclarenapp/ui/settings/SettingsFragment.java
+++ app-build/app/src/main/java/com/gabrielmiguelpedro/maclarenapp/ui/settings/SettingsFragment.java
@@ -1,9 +1,12 @@
 package com.gabrielmiguelpedro.maclarenapp.ui.settings;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +15,37 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.gabrielmiguelpedro.maclarenapp.MainActivity;
 import com.gabrielmiguelpedro.maclarenapp.R;
 
 public class SettingsFragment extends Fragment {
-
+    private MainActivity callback;
     private SettingsViewModel settingsViewModel;
+    private Button button_alterar;
+    private EditText editText;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        callback = (MainActivity) context;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        settingsViewModel.getText().observe(this, new Observer<String>() {
+        button_alterar = root.findViewById(R.id.btn_si_alterar);
+        editText = root.findViewById(R.id.si_email_alterar);
+
+
+        button_alterar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+
             }
         });
+
         return root;
     }
 }
