17b62ca8faad5db2ce3fe629f42ff41cf4676b0e
nhenheca <33757448+nhenheca@users.noreply.github.com>
2020-02-16 03:39:14 +0000

    BabyCarDialog 2Faces

--- app-build/app/src/main/java/com/gabrielmiguelpedro/maclarenapp/BabyCarDialog.java
+++ app-build/app/src/main/java/com/gabrielmiguelpedro/maclarenapp/BabyCarDialog.java
@@ -21,7 +21,9 @@ public class BabyCarDialog extends AppCompatDialogFragment {
     private MainActivity callback;
     private int markerId;
     private long rowId;
-    private long intreval;
+    private double d;
+    private int BabyCarUse;
+    private int isUsing;
 
     public BabyCarDialog(Bundle bundle) {
         this.bundle = bundle;
@@ -36,57 +38,62 @@ public class BabyCarDialog extends AppCompatDialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getContext(), R.style.AlertDialogCustom));
-        builder.setTitle("Alugar Carrinho - " + bundle.getString("markerId"))
-                .setMessage("Pretende Alugar este Carrinho?")
-                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //funciona
-                        String id = bundle.getString("markerId");
-                        id = id.substring(1);
-                        markerId = Integer.parseInt(id);
-                        markerId++;
+        final AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getContext(), R.style.AlertDialogCustom));
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //funciona
+                String id = bundle.getString("markerId");
+                id = id.substring(1);
+                markerId = Integer.parseInt(id);
+                markerId++;
 
-                        double lon1 = bundle.getDouble("lon");
-                        double lat1 = bundle.getDouble("lat");
-                        double lon2 = bundle.getDouble("markerLon");
-                        double lat2 = bundle.getDouble("markerLat");
+                double lon1 = bundle.getDouble("lon");
+                double lat1 = bundle.getDouble("lat");
+                double lon2 = bundle.getDouble("markerLon");
+                double lat2 = bundle.getDouble("markerLat");
 
-                        double d = distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2);
+                d = distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2);
 
-                        int BabyCarUse = callback.db.getUseByIdBabyCar(markerId);
+                BabyCarUse = callback.db.getUseByIdBabyCar(markerId);
 
-                        int isUsing = callback.getDb().getIsUsingById(callback.getUser().getUserID());
+                isUsing = callback.getDb().getIsUsingById(callback.getUser().getUserID());
 
-                        Toast.makeText(getContext(), "D: " + d + " Car: " + BabyCarUse + " User:" + isUsing, Toast.LENGTH_LONG).show();
-                        if (d <= 10 && BabyCarUse == 0 && isUsing == 0) {
+                Toast.makeText(getContext(), "D: " + d + " Car: " + BabyCarUse + " User:" + isUsing, Toast.LENGTH_LONG).show();
+                if (d <= 10 && BabyCarUse == 0 && isUsing == 0) {
 
-                            Date date = new Date();
-                            Toast.makeText(getContext(), "Car: " + callback.db.getUseByIdBabyCar(markerId) + " User: " + callback.getDb().getIsUsingById(callback.getUser().getUserID()), Toast.LENGTH_LONG).show();
+                    Date date = new Date();
+                    Toast.makeText(getContext(), "Car: " + callback.db.getUseByIdBabyCar(markerId) + " User: " + callback.getDb().getIsUsingById(callback.getUser().getUserID()), Toast.LENGTH_LONG).show();
 
-                            callback.getDb().setIsUsing(1, callback.getUser());//colocar o utilizador em uso!!
-                            callback.getDb().setIsUseCar(1, String.valueOf(markerId));//colocar o carro em uso!!
-                            rowId = callback.getDb().addHistoric(new Historic(0, date, callback.getUser(), callback.getDb().getBabyCarById(markerId)));
+                    callback.getDb().setIsUsing(1, callback.getUser());//colocar o utilizador em uso!!
+                    callback.getDb().setIsUseCar(1, String.valueOf(markerId));//colocar o carro em uso!!
+                    rowId = callback.getDb().addHistoric(new Historic(0, date, callback.getUser(), callback.getDb().getBabyCarById(markerId)));
 
-                            getActivity().startService(new Intent(getActivity(), MyService.class));//chamar o service
+                    getActivity().startService(new Intent(getActivity(), MyService.class));//chamar o service
 
-                            Toast.makeText(getContext(), "Car: " + callback.db.getUseByIdBabyCar(markerId) + " User: " + callback.getDb().getIsUsingById(callback.getUser().getUserID()), Toast.LENGTH_LONG).show();
-                            Toast.makeText(getContext(), "RowId: " + rowId, Toast.LENGTH_LONG).show();
+                    Toast.makeText(getContext(), "Car: " + callback.db.getUseByIdBabyCar(markerId) + " User: " + callback.getDb().getIsUsingById(callback.getUser().getUserID()), Toast.LENGTH_LONG).show();
+                    Toast.makeText(getContext(), "RowId: " + rowId, Toast.LENGTH_LONG).show();
 
-                        } else if(isUsing==1 && d <= 10){
-                            Toast.makeText(getContext(), "Acabar Aluger?", Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(getContext(), "Nãoi é possivel Alugar", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                })
-                .setNegativeButton("Não", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //funciona
-                    }
-                });
+                } else if (isUsing == 1 && d <= 10) {
+                    Toast.makeText(getContext(), "Acabar Aluger?", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), "Nãoi é possivel Alugar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        })
+        .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //funciona
+            }
+        });
+        if (callback.getDb().getIsUsingById(callback.getUser().getUserID()) == 1) {
+            builder.setTitle("Finalizar Aluger? - " + bundle.getString("markerId"))
+            .setMessage("Finalizar este Aluger?");
+        }else{
+            builder.setTitle("Alugar Carrinho - " + bundle.getString("markerId"))
+            .setMessage("Pretende Alugar este Carrinho?");
+        }
         return builder.create();
     }
 
